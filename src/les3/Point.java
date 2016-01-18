e009d5bd34a42197ee0226719778390b640b8c26
mira <mturakulov@gmail.com>
2016-01-18 16:44:23 +0200

    point

--- src/les3/Point.java
+++ src/les3/Point.java
@@ -3,10 +3,13 @@ package les3;
 public class Point {
 	
 	public static void main(String[] args) {
-		double x, y;
 		
-		
-		
+		System.out.println(Square(5,5,5));
+	}
+	
+	public static double Square(double a, double b, double c) {
+		double result = (a + b + c) / 2;
+		return result;
 	}
 
 }
--- src/les5/ArraysOne.java
+++ src/les5/ArraysOne.java
@@ -2,6 +2,10 @@ package les5;
 
 public class ArraysOne {
 	public static void main(String[] args) {
+		String[] str = new String[] {"nick", "git", "comp", "nick", "one", "git"};
 		
+		for(int i =0; i < str.length; i++) {
+			str[i] = str[i] ? str[i] : null;
+		}
 	}
 }
